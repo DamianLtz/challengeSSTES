55ecc0c2a8dfa4a1918a4fc7cd132e90de899de2
DamianLtz <damianlaterza@gmail.com>
2023-06-19 15:46:13 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -2,10 +2,7 @@ const toggleButton = document.querySelector("#menu");
 
 const menuVisible = document.querySelector(".navbar__menu__options");
 
-console.log(menuVisible);
-
 toggleButton.addEventListener("click", () => {
-  console.log("hola");
   if (menuVisible.classList.contains("menu-visible")) {
     menuVisible.classList.remove("menu-visible");
   } else {
